5eb4cab4eeb6a4a80f4245177d3df5e0678f2924
Hugo Chinchilla <hugochenr@gmail.com>
2023-04-21 00:41:18 -0600

    Agrega archivo de database y remueve elementos fallidos de onboarding

--- src/nova-components/Onboarding/resources/js/components/Card.vue
+++ src/nova-components/Onboarding/resources/js/components/Card.vue
@@ -1,19 +1,67 @@
 <template>
+  <div>
+    <VOnboardingWrapper ref="wrapper" :steps="steps" />
+  </div>
 </template>
-
 <script>
-export default {
-  props: [
-    'card',
-
-    // The following props are only available on resource detail cards...
-    // 'resource',
-    // 'resourceId',
-    // 'resourceName',
-  ],
-
-  mounted() {
-    alert('onboarding mounted');
+import { defineComponent, ref } from 'vue'
+import { VOnboardingWrapper, useVOnboarding } from 'v-onboarding'
+import 'v-onboarding/dist/style.css'
+export default defineComponent ({
+  components: {
+    VOnboardingWrapper
   },
-}
+  setup() {
+    let novaElement = document.getElementById('nova');
+    let sidebarElement = document.getElementsByClassName('sidebar-item');
+    novaElement.setAttribute('element', 'nova');
+    console.log(novaElement);
+    console.log(sidebarElement);
+
+    for(var i in sidebarElement) {
+      if(sidebarElement[i].getAttribute) {
+        sidebarElement[i].setAttribute('id', 'step1');
+        sidebarElement[i].setAttribute('ref', 'step1');
+      }
+    }
+
+
+
+    const wrapper = ref(null)
+    const { start, goToStep, finish } = useVOnboarding(wrapper)
+    const steps = [
+      {
+        attachTo: { element: '#step1' },
+        content: { title: "Welcome!" }
+      },
+      {
+        attachTo: { element: '#bar' },
+        content: {
+          title: "Do it!",
+          description: "This is a description field and I really don't know what to write here..."
+        }
+      }
+    ]
+
+    return {
+      wrapper,
+      steps,
+      start,
+      finish,
+      goToStep
+    }
+  },
+  beforeMount() {
+
+  },
+  onMount(){
+    this.$refs.wrapper.start();
+  }
+})
+
+
 </script>
+
+<style>
+
+</style>
---
+++ src/nova-components/StepOnboarding/resources/js/pages/Tool.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Head title="Step Onboarding" />
+
+    <Heading class="mb-6">Users</Heading>
+
+    <input type="button" value="button"/>
+
+    <Card
+      class="flex flex-col items-center justify-center"
+      style="min-height: 300px"
+    >
+      
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    //
+  },
+}
+</script>
+
+<style>
+/* Scoped Styles */
+</style>
